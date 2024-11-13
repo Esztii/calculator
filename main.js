eb397f271c4f7fd72dfd32ba7ee4e20b1ce9a2f6
Eszti <eszti.orosz@gmail.com>
2024-11-13 09:41:34 +0100

    asd

--- main.js
+++ main.js
@@ -1,3 +1,30 @@
 document.getElementById('icon').addEventListener('click', () => {
-    document.getElementById('calculator').classList.toggle('visible')
+    document.getElementById('calculator').classList.add('visible')
+    window.addEventListener('keydown', (e) => {
+        console.log("asd")
+        handleKeydown(e)
+    } )
 } )
+
+document.getElementById('x').addEventListener('click', () => {
+    document.getElementById('calculator').classList.remove('visible')
+    document.removeEventListener('keydown', handleKeydown )
+
+})
+
+function handleKeydown(e){
+    switch(e.key){
+        case "0": fireclick("nulla"); break;
+
+    }
+}
+
+
+function fireclick(buttonId){ 
+    button = document.getElementById(buttonId);
+    button.classList.add('clicked');
+    setTimeout(() => {
+        button.classList.remove('clicked');
+    }, 200) 
+}
+
